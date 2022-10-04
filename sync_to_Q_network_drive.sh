046f32e0207a6dde1bc17804ecc3e731f0309937
Hans Wessels <nyh@suiker.org>
2022-10-04 16:21:14 +0200

    local dev scripts for syncing code to/from target machines. Now use `rsync` instead of dreadful `cp -r`.

---
+++ sync_to_Q_network_drive.sh
@@ -0,0 +1,10 @@
+#! /bin/bash
+
+rm -rf .git
+#cp -r ./* ~/q/nyh/gup/ 
+#
+#cat config.guess > ~/q/nyh/gup/config.guess
+#cat config.sub   > ~/q/nyh/gup/config.sub
+
+rsync -v -r --exclude=.git/ '--exclude=*.[ao]' '--exclude=*.l[ao]' '--exclude=*.bak' '--exclude=*.i' '--exclude=*~' '--exclude=autom4te.cache/*'   -u -t -U --copy-links  ./ ~/q/nyh/gup/
+
