79a75ab82b244caafeb159430bd20ed308ebfa00
Hans Wessels <nyh@suiker.org>
2022-10-23 17:23:22 +0200

    updated reverse-sync script + makefile test target

--- sync_to_Q_network_drive.sh
+++ sync_to_Q_network_drive.sh
@@ -7,7 +7,4 @@ rm -rf .git
 #cat config.sub   > ~/q/nyh/gup/config.sub
 
 #rsync -r -I -U -t --copy-links -i -c --ignore-existing --stats --progress '--exclude=.git/' '--exclude=*.[ao]' '--exclude=*.l[ao]' '--exclude=*.bak' '--exclude=*.i' '--exclude=*~' '--exclude=autom4te.cache/*'   . ~/q/nyh/Atari/e/pc/gup/
-#rsync -a -v -P --copy-links -c --stats --progress '--exclude=.git/' '--exclude=*.[ao]' '--exclude=*.l[ao]' '--exclude=*.bak' '--exclude=*.i' '--exclude=*~' '--exclude=autom4te.cache/*'   . ~/q/nyh/Atari/e/pc/gup/
-rsync -a -v -P --copy-links -c --stats --progress    . ~/q/nyh/Atari/e/pc/gup/
-#rsync -r -t --copy-links -c --stats --progress '--exclude=.git/'    . ~/q/nyh/Atari/e/pc/gup/
-#cp -r .     ~/q/nyh/Atari/e/pc/gup/
+rsync -a -v -P --copy-links -c --stats --progress '--exclude=.git/' '--exclude=*.[ao]' '--exclude=*.l[ao]' '--exclude=*.bak' '--exclude=*.i' '--exclude=*~' '--exclude=autom4te.cache/*'   . ~/q/nyh/Atari/e/pc/gup/
