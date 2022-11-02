2c0809c81a0b77b4dc56740aca070f09533f1471
Ger Hobbelt <ger@hobbelt.com>
2022-11-02 10:36:46 +0100

    add a quick github sync-to-both-repos shell script to reduce timing and mistakes. ( https://github.com/HansWessels/gup/issues/11 ) ( script ripped from my own Qiqqa work & repurposed )

---
+++ git_push_all.sh
@@ -0,0 +1,21 @@
+#! /bin/bash
+#
+
+cat <<EOF
+
+>>> Pushing our work to both remotes...
+
+EOF
+for f in GerHobbelt HansWessels ; do 
+	echo $f 
+	git push --all git@github.com:$f/gup.git
+done
+
+# fetch all remote work as an afterthought
+cat <<EOF
+
+
+>>> Fetching all remote work...
+
+EOF
+git fetch --all 
