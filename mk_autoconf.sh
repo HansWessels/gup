35e118e95a68e920b448a4059f0d4bfd9848fe39
Hans Wessels <nyh@suiker.org>
2023-03-16 00:49:12 +0100

    patch gnu configure regen script; cf. https://github.com/easybuilders/easybuild-easyblocks/issues/1276

--- mk_autoconf.sh
+++ mk_autoconf.sh
@@ -8,7 +8,8 @@ rm config.*
 rm libtool
 
 autoconf
-automake --add-missing
+# automake --add-missing
+automake --add-missing --copy --force-missing
 autoheader
 
 # fix for git + Windows: ditch the symbolic links that may have been produced during the process above:
