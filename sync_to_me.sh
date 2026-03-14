c35bfe1ab9c93600be55f00b844841447dc7ba90
H. Wessels <github@suiker.org>
2026-03-14 01:44:42 +0100

    Turned of verbose mode

--- sync_to_me.sh
+++ sync_to_me.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
 # cp -r ~/q/nyh/Atari/e/pc/gup/ ../ ; rm -rf .git
-rsync -v -r --exclude=.git/ '--exclude=test/Mame/*' '--exclude=*.[ao]' '--exclude=*.l[ao]' '--exclude=*.bak' '--exclude=*.i' '--exclude=*.obj' '--exclude=*~' '--exclude=autom4te.cache/*' '--exclude=.vs' '--exclude=*.pdb' '--exclude=*.*log' -u -t --copy-links   ~/q/nyh/Atari/e/pc/gup/ ./
+rsync -r --exclude=.git/ '--exclude=test/Mame/*' '--exclude=*.[ao]' '--exclude=*.l[ao]' '--exclude=*.bak' '--exclude=*.i' '--exclude=*.obj' '--exclude=*~' '--exclude=autom4te.cache/*' '--exclude=.vs' '--exclude=*.pdb' '--exclude=*.*log' -u -t --copy-links   ~/q/nyh/Atari/e/pc/gup/ ./
